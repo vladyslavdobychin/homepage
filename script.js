b487acbb858fdc00848c1bf0da4ea7ec5a1f52f5
Vladyslav Dobychin <vladyslavdobychin@gmail.com>
2023-06-18 18:32:23 +0300

    Add JS logic for the element of surpise

--- script.js
+++ script.js
@@ -5,21 +5,26 @@ const themeBtnLight = document.getElementById('theme-btn-light');
 const darkTehemSupremacyTextLi = document.getElementById(
   'dark-theme-supremacy'
 );
+const elementOfSurpriseButton = document.querySelector('.surprise__btn');
+const modalWrapper = document.querySelector('.modal-wrapper');
+const videoPlayer = document.getElementById('video-player');
 const body = document.querySelector('body');
 
 const toggleTheme = (newTheme) => {
-  themeBtnDark.classList.toggle('change-theme__btn--hidden');
-  themeBtnLight.classList.toggle('change-theme__btn--hidden');
+  themeBtnDark.classList.toggle('visually-hidden');
+  themeBtnLight.classList.toggle('visually-hidden');
   body.dataset.theme = newTheme;
   changeDarkTehemSupremacyText(newTheme);
 };
 
 const changeDarkTehemSupremacyText = (theme) => {
-    if (theme === 'dark') {
-        darkTehemSupremacyTextLi.textContent = "Always prefere a dark theme, you did a good job changing it 👍";
-    } else {
-        darkTehemSupremacyTextLi.textContent = "Always prefere a dark theme, please change it up there 👆";
-    }
+  if (theme === 'dark') {
+    darkTehemSupremacyTextLi.textContent =
+      'Always prefere a dark theme, you did a good job changing it 👍';
+  } else {
+    darkTehemSupremacyTextLi.textContent =
+      'Always prefere a dark theme, please change it up there 👆';
+  }
 };
 
 themeBtnDark.addEventListener('click', (e) => {
@@ -30,4 +35,21 @@ themeBtnDark.addEventListener('click', (e) => {
 themeBtnLight.addEventListener('click', (e) => {
   const clickedBtn = e.target.closest('.change-theme__btn');
   toggleTheme(clickedBtn.dataset.theme);
+});
+
+elementOfSurpriseButton.addEventListener('click', function () {
+  modalWrapper.classList.remove('visually-hidden');
+  videoPlayer.play();
+  document.body.style.overflow = 'hidden';
+});
+
+modalWrapper.addEventListener('click', function (e) {
+  if (e.target.id === 'video-player') {
+    return;
+  }
+
+  modalWrapper.classList.add('visually-hidden');
+  videoPlayer.pause();
+  videoPlayer.currentTime = 0;
+  document.body.style.overflow = 'auto';
 });
