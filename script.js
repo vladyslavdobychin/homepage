abd4a9eb3563cdff7d9f8fa5a605fe718b3f6557
Vladyslav Dobychin <vladyslavdobychin@gmail.com>
2023-06-18 18:57:56 +0300

    Refactored JS logic

--- script.js
+++ script.js
@@ -1,55 +1,80 @@
 'use strict';
 
-const themeBtnDark = document.getElementById('theme-btn-dark');
-const themeBtnLight = document.getElementById('theme-btn-light');
-const darkTehemSupremacyTextLi = document.getElementById(
-  'dark-theme-supremacy'
-);
-const elementOfSurpriseButton = document.querySelector('.surprise__btn');
-const modalWrapper = document.querySelector('.modal-wrapper');
-const videoPlayer = document.getElementById('video-player');
-const body = document.querySelector('body');
+class ThemeToggler {
+  constructor(darkBtnId, lightBtnId) {
+    this.darkBtn = document.getElementById(darkBtnId);
+    this.lightBtn = document.getElementById(lightBtnId);
+    this.darkThemeSupremacyLi = document.getElementById('dark-theme-supremacy');
+    this.body = document.body;
 
-const toggleTheme = (newTheme) => {
-  themeBtnDark.classList.toggle('visually-hidden');
-  themeBtnLight.classList.toggle('visually-hidden');
-  body.dataset.theme = newTheme;
-  changeDarkTehemSupremacyText(newTheme);
-};
-
-const changeDarkTehemSupremacyText = (theme) => {
-  if (theme === 'dark') {
-    darkTehemSupremacyTextLi.textContent =
-      'Always prefere a dark theme, you did a good job changing it 👍';
-  } else {
-    darkTehemSupremacyTextLi.textContent =
-      'Always prefere a dark theme, please change it up there 👆';
-  }
-};
-
-themeBtnDark.addEventListener('click', (e) => {
-  const clickedBtn = e.target.closest('.change-theme__btn');
-  toggleTheme(clickedBtn.dataset.theme);
-});
-
-themeBtnLight.addEventListener('click', (e) => {
-  const clickedBtn = e.target.closest('.change-theme__btn');
-  toggleTheme(clickedBtn.dataset.theme);
-});
-
-elementOfSurpriseButton.addEventListener('click', function () {
-  modalWrapper.classList.remove('visually-hidden');
-  videoPlayer.play();
-  document.body.style.overflow = 'hidden';
-});
-
-modalWrapper.addEventListener('click', function (e) {
-  if (e.target.id === 'video-player') {
-    return;
+    this.darkBtn.addEventListener('click', () => this.toggleTheme('dark'));
+    this.lightBtn.addEventListener('click', () => this.toggleTheme('light'));
   }
 
-  modalWrapper.classList.add('visually-hidden');
-  videoPlayer.pause();
-  videoPlayer.currentTime = 0;
-  document.body.style.overflow = 'auto';
-});
+  toggleTheme(newTheme) {
+    this.darkBtn.classList.toggle('visually-hidden');
+    this.lightBtn.classList.toggle('visually-hidden');
+    this.body.dataset.theme = newTheme;
+    this.changeDarkThemeSupremacyText(newTheme);
+  }
+
+  changeDarkThemeSupremacyText(theme) {
+    if (theme === 'dark') {
+      this.darkThemeSupremacyLi.textContent =
+        'Always prefer a dark theme, you did a good job changing it 👍';
+    } else {
+      this.darkThemeSupremacyLi.textContent =
+        'Always prefer a dark theme, please change it up there 👆';
+    }
+  }
+}
+
+class ElementOfSurprise {
+  constructor(buttonSelector, modalSelector, videoSelector) {
+    this.button = document.querySelector(buttonSelector);
+    this.modal = document.querySelector(modalSelector);
+    this.video = document.querySelector(videoSelector);
+
+    this.button.addEventListener('click', () => this.openModal());
+    this.modal.addEventListener('click', (e) => this.closeModal(e));
+  }
+
+  openModal() {
+    this.modal.classList.remove('visually-hidden');
+    this.video.play();
+    document.body.style.overflow = 'hidden';
+  }
+
+  closeModal(e) {
+    if (e.target.id === 'video-player') {
+      return;
+    }
+    this.modal.classList.add('visually-hidden');
+    this.video.pause();
+    this.video.currentTime = 0;
+    document.body.style.overflow = 'auto';
+  }
+}
+
+class App {
+  constructor() {
+    this.themeToggler = null;
+    this.elementOfSurprise = null;
+  }
+
+  init() {
+    this.themeToggler = new ThemeToggler(
+      'theme-btn-dark',
+      'theme-btn-light',
+    );
+    this.surpriseVideo = new ElementOfSurprise(
+      '.surprise__btn',
+      '.modal-wrapper',
+      '#video-player'
+    );
+  }
+}
+
+// Usage
+const app = new App();
+app.init();
